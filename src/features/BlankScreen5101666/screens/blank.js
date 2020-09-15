6f42c3847335b0f2adb3b32ea9122fa2bed05234
Crowdbotics <team@crowdbotics.com>
2020-09-15 18:20:46 +0000

    Update components

--- src/features/BlankScreen5101666/screens/blank.js
+++ src/features/BlankScreen5101666/screens/blank.js
@@ -359,47 +359,49 @@ export class _Blank extends React.Component {
         contentSample text contentSample text contentSample text contentSample
         text content
       </Text>
-      <Button
-        textStyle={{
-          fontSize: 12,
-          color: "#000000",
-          textAlign: "center",
-          fontFamily: "Roboto-Regular"
-        }}
-        style={{
-          width: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 5,
-          marginBottom: 5,
-          paddingLeft: 5,
-          paddingRight: 5,
-          paddingTop: 5,
-          paddingBottom: 5,
-          overflow: "visible",
-          fontSize: 12,
-          color: "#000000",
-          backgroundColor: "#3366FF",
-          fontFamily: "Roboto-Regular",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 0,
-          borderLeftWidth: 0,
-          borderRightWidth: 0,
-          borderTopWidth: 0,
-          borderBottomWidth: 0,
-          borderRadius: 0,
-          textAlign: "center",
-          textAlignVertical: "center",
-          textDecorationLine: "none",
-          textTransform: "none",
-          lineHeight: 12,
-          letterSpacing: 0
-        }}
-        onPress={() => alert("Pressed!")}
-      >
-        Return to List
-      </Button>
+      <TouchableOpacity onPress={() => this.props.navigation.navigate("")}>
+        <Button
+          textStyle={{
+            fontSize: 12,
+            color: "#000000",
+            textAlign: "center",
+            fontFamily: "Roboto-Regular"
+          }}
+          style={{
+            width: "100%",
+            marginLeft: 0,
+            marginRight: 0,
+            marginTop: 5,
+            marginBottom: 5,
+            paddingLeft: 5,
+            paddingRight: 5,
+            paddingTop: 5,
+            paddingBottom: 5,
+            overflow: "visible",
+            fontSize: 12,
+            color: "#000000",
+            backgroundColor: "#3366FF",
+            fontFamily: "Roboto-Regular",
+            borderColor: "#000000",
+            borderStyle: "solid",
+            borderWidth: 0,
+            borderLeftWidth: 0,
+            borderRightWidth: 0,
+            borderTopWidth: 0,
+            borderBottomWidth: 0,
+            borderRadius: 0,
+            textAlign: "center",
+            textAlignVertical: "center",
+            textDecorationLine: "none",
+            textTransform: "none",
+            lineHeight: 12,
+            letterSpacing: 0
+          }}
+          onPress={() => alert("Pressed!")}
+        >
+          Return to List
+        </Button>
+      </TouchableOpacity>
     </View>
   )
 }
